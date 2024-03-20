fc0cb4cb0e13c8c343055e4cbcdeb03da19b94cc
marvinkaldewey <marvin.kaldewey@gmx.de>
2024-03-20 20:07:32 +0100

    Synset implementation

--- brontology/graph/entity.py
+++ brontology/graph/entity.py
@@ -1,11 +1,28 @@
 # noinspection PyUnresolvedReferences
-from typing import Union, Optional
-
+from typing import Union, Optional, List, Tuple
+from random import choice
+from math import sqrt
 from brontology.graph.node import Node, Link
 
+Vector = Tuple[float]
 
 class Synset:
-    ...
+    def __init__(self, embeddings: List[Vector]):
+        self.embeddings = embeddings
+        self.center = self.calculate_center()
+        self.repr = self.get_representative_word()
+
+    def calculate_center(self) -> Vector:
+        # Berechnung des Zentrums der Vektoren
+        return tuple(sum(x) / len(x) for x in zip(*self.embeddings))
+
+    def get_representative_word(self) -> str:
+        # Auswahl eines repräsentativen Wortes (in diesem Fall zufällig)
+        return str(choice(self.embeddings))
+
+    def distance(self, other_synset: "Synset") -> float:
+        # Berechnung der euklidischen Distanz zu einem anderen Synset
+        return sqrt(sum((c1 - c2) ** 2 for c1, c2 in zip(self.center, other_synset.center)))
 
 
 class Entity(Node["Relation"]):
