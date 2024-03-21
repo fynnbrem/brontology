39388dcb90186b43e605d5f1d02d7d5d5ec904f0
fynnb <fynn.bremser@gmx.de>
2024-03-21 23:52:15 +0100

    added passive voice extraction

---
+++ brontology/relation_extraction/relation_types/passive_voice.py
@@ -0,0 +1,39 @@
+# noinspection PyUnresolvedReferences
+from typing import Union, Optional
+
+from spacy.symbols import auxpass, agent, nsubjpass, pobj
+from spacy.tokens import Token
+
+from brontology.relation_extraction.types import TokenRelation
+
+
+def extract_passive_voice_relation(verb: Token) -> Optional[TokenRelation]:
+    if not any(child.dep == auxpass for child in verb.children):
+        # Filter: The verb is no passive if it lacks an auxiliary passive verb.
+        return None
+
+    # ↓ Find the tail. It might be tailless if it has no agent.
+    verb_agent: Optional[Token] = None
+    for child in verb.children:
+        if child.dep == agent:
+            verb_agent = child
+            break
+
+    tail: Optional[Token] = None
+    if verb_agent is not None:
+        for child in verb_agent.children:
+            if child.dep == pobj:
+                tail = child
+                break
+
+    # ↓ Find the head. It might be headless if it has no nominal subject.
+    head: Optional[Token] = None
+    for child in verb.children:
+        if child.dep == nsubjpass:
+            head = child
+            break
+
+    return TokenRelation(tail, verb, head)
+
+
+
