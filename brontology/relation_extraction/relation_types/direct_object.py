680c97ec0c7b177d64865477c645a90cc09dc28d
marvinkaldewey <marvin.kaldewey@gmx.de>
2024-04-04 23:50:33 +0200

    Last Meeting changes

--- brontology/relation_extraction/relation_types/direct_object.py
+++ brontology/relation_extraction/relation_types/direct_object.py
@@ -1,16 +1,23 @@
+from brontology.relation_extraction.model import TokenRelation
+
+
 def extract_direct_object_relation(verb):
     # Initialisiere das Subjekt und das direkte Objekt mit "NONE"
-    dobj = "NONE"
-    subj = "NONE"
+    dobj = None
+    subj = None
 
     # Durchlaufe die Kinder des Verbs
     for child in verb.children:
         # Überprüfe, ob die Beziehung ein direktes Objekt ist
         if child.dep_ == "dobj":
-            dobj = child.text
+            dobj = child
         # Überprüfen, ob die Beziehung ein Subjekt ist
-        elif child.dep_ in ["nsubj", "nsubjpass"]:
-            subj = child.text
-
-    # Füge das direkte Objekt und das Subjekt der Liste hinzu, unabhängig davon, ob sie gefunden wurden oder nicht
-    results.append([verb.sent, verb.text, dobj, subj])
+        elif child.dep_ in ["nsubj"]:
+            subj = child
+    if dobj is None:
+        return None
+    if subj is None:
+        return None
+    
+    return TokenRelation(subj, verb, dobj)
+    
