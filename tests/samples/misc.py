b368a3eeaf8149c1d5c776b21ce96b0f56ff1f96
fynnb <fynn.bremser@gmx.de>
2024-08-01 00:11:09 +0200

    Added another fake link

--- tests/samples/misc.py
+++ tests/samples/misc.py
@@ -3,6 +3,7 @@
 FAKE_LINKS = [
     "https://en.wikipedia.org/wiki/Lizard",
     "https://en.wikipedia.org/wiki/Gecko",
+    "https://en.wikipedia.org/wiki/Chameleon",
     "https://en.wikipedia.org/wiki/Insect",
     "https://en.wikipedia.org/wiki/Moth",
     "https://en.wikipedia.org/wiki/Mosquito",
