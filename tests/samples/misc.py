6b1c3b9bed12df942f29717162c9fd9c94a0988b
Fynn Bremser <FBremser@dspace.de>
2024-08-01 16:24:39 +0200

    added new prettify for iterables

--- tests/samples/misc.py
+++ tests/samples/misc.py
@@ -7,5 +7,8 @@ FAKE_LINKS = [
     "https://en.wikipedia.org/wiki/Insect",
     "https://en.wikipedia.org/wiki/Moth",
     "https://en.wikipedia.org/wiki/Mosquito",
+    "https://en.wikipedia.org/wiki/Dolphin",
+    "https://en.wikipedia.org/wiki/Orca",
+    "https://en.wikipedia.org/wiki/Fish",
 ]
 FAKE_LINK = FAKE_LINKS[0]
